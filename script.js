b5735e5ca34ef0d304d01fa55f6b59768c4ba851
Wijanay <wijayagalif@gmail.com>
2024-10-17 21:01:21 +0700

    update theme, responsive device, switch theme button, dan script dark theme mode

--- script.js
+++ script.js
@@ -4,4 +4,18 @@ document.addEventListener("DOMContentLoaded", function() {
         item.style.animationDelay = `${index * 0.1}s`;
         item.classList.add("fadeIn");
     });
+
+    const themeToggleButton = document.getElementById("theme-toggle");
+    themeToggleButton.addEventListener("click", () => {
+        document.body.classList.toggle("dark");
+        items.forEach(item => {
+            item.classList.toggle("dark");
+        });
+        const header = document.querySelector("header");
+        header.classList.toggle("dark");
+        const footer = document.querySelector("footer");
+        footer.classList.toggle("dark");
+
+    });
+
 });
